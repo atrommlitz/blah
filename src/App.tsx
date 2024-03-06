31d7fcd68de8438e304dfcdbb2192f84d9f33b1b
atrommlitz <119766231+atrommlitz@users.noreply.github.com>
2024-03-06 09:09:58 -0700

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// Take in the json data that I have been given
+import CollegeBasketballTeams from './CollegeBasketballTeams.json';
+
+//H1 function tag for the website
+function Welcome() {
+  return <h1>NCAA Basketball Tournament Teams</h1>;
+}
+
+interface Team {
+  tid: number;
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+interface TeamCardProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function TeamCard({ school, name, city, state }: TeamCardProps) {
+  return (
+    <div className="team-card">
+      <h2 className="school-name">{school}</h2>
+      <p className="mascot-name">{name}</p>
+      <p className="location">
+        Location: {city}, {state}
+      </p>
+    </div>
+  );
+}
+
+function TeamList() {
+  const teams: Team[] = CollegeBasketballTeams.teams;
+
+  return (
+    <div>
+      <h1>List of Teams</h1>
+      <div className="team-list">
+        {teams.map((team) => (
+          <TeamCard
+            key={team.tid}
+            school={team.school}
+            name={team.name}
+            city={team.city}
+            state={team.state}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello</p>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
